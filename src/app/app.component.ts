f7f7e6d76ecdcf6e5c37e31033c47452485940a4
Gael Gomez <code@gael.cc>
2024-01-04 00:13:23 -0600

    build test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import anime from 'animejs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portfolio';
+  followMouse(e: MouseEvent) {}
 }
